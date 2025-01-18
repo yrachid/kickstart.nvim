e7b3a8bde7cec3d585ba58d840a8e19069432ead
yrachid <anuar.yasser@gmail.com>
2025-01-17 22:48:20 -0300

    Enable ruby lsp

--- init.lua
+++ init.lua
@@ -587,6 +587,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- ruby_lsp = {},
+        -- rubocop = {},
         tsserver = {
           capabilities = lspCapabilities,
         },
