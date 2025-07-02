62686f093a723da6726766dc01ec6a312856654e
yrachid <anuar.yasser@gmail.com>
2025-07-02 18:39:19 -0300

    organize colorschemes

--- init.lua
+++ init.lua
@@ -775,34 +775,11 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- start:colorschemes
+  { 'folke/tokyonight.nvim' },
   {
     'sainnhe/gruvbox-material',
     lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      -- vim.g.gruvbox_material_enable_italic = true
-      -- vim.g.gruvbox_material_background = 'medium'
-      -- vim.cmd.colorscheme 'gruvbox-material'
-    end,
   },
   {
     'thallada/farout.nvim',
@@ -813,10 +790,11 @@ require('lazy').setup({
     'shmerl/neogotham',
     lazy = false,
     config = function()
-      require('neogotham'):setup { oldgotham = true }
       vim.cmd.colorscheme 'neogotham'
     end,
   },
+  -- end:colorschemes
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
